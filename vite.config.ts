0fe811dab6e6b9a1cc4234ce61eaec154000adeb
Konijnebeer <98305413+Konijnebeer@users.noreply.github.com>
2025-12-25 00:50:00 +0100

    SPA fix

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
+  base: '/er-ttrpg-app/',
   plugins: [
     devtools(),
     tanstackRouter({
