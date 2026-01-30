93bff341280e868b7568aac4bd6b7dc565c6c54f
Konijnebeer <Konijnebeer@users.noreply.github.com>
2026-01-30 15:43:37 +0000

    deploy: 35d8f2e3f7c8a839a229cbc925adc26b2a19b400

---
+++ assets/_characterId.edit-DDxMs_km.js
@@ -0,0 +1,1 @@
+import{bb as t,j as e}from"./index-De3IN0Sk.js";function r(){const{characterId:o}=t.useParams();return e.jsxs("div",{children:["Hello ",o," edit"]})}export{r as component};
--- assets/create-CfWdwZAP.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-QXhXNQW-.js";function t(){return o.jsx("div",{children:'Hello "/sources/create"!'})}export{t as component};
